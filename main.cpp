ff123535a9758d3343575f7965cd887278a30835
justen <justenc94@gmail.com>
2019-06-18 21:17:07 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 03
+Purpose:
+***********************************************************/
+#include "main.h"
+
+int main() {
+
+    std::cout << "Hello world!" << std::endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 03
+Purpose:
+***********************************************************/
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+
+#endif //MAIN_H
