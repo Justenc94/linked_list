ba4fe2e08141e596aec2ea4f90180b724fc79b6b
justen <justenc94@gmail.com>
2019-06-19 22:23:10 -0500

    Added header and cpp files for:
    list
    
    functions

---
+++ functions.h
@@ -0,0 +1,9 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 03
+Purpose:
+***********************************************************/
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#endif //FUNCTIONS_H
