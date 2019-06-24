22ee4e2ac708de68bf8967f3b06f73a390ce4cd3
justen <justenc94@gmail.com>
2019-06-23 21:20:55 -0500

    built addMiddle method

--- list.cpp
+++ list.cpp
@@ -11,25 +11,29 @@ List::List() {
 }
 
 //********************************************PUBLIC METHODS********************************************
-bool List::addNode(int new_id, string new_data){
+bool List::addNode(int new_id, string new_data) {
     Node *temp_node = head;
-    if(searchIds(new_id) != -1){
+    if (searchIds(new_id) != -1) {
         Node *new_node = new Node;
         new_node->id = new_id;
         new_node->data = new_data;
-        if(temp_node == nullptr || temp_node->id > new_node->id){
+        if (temp_node == nullptr || temp_node->id > new_id) {
             addHead(new_node);
             return true;
-        }else{
-            while(temp_node->next != nullptr && temp_node->next->id < new_node->id){
-                addTail(new_node);
-                return true;
+        } else {
+            for (int i = 0; i < count; i++) {
+                while(temp_node->next == nullptr){
+                    addTail(new_node);
+                    return true;
+                }
+                while(new_id > temp_node->id && new_id < temp_node->next->id){
+                    addMiddle(new_node);
+                    return true;
+                }
+                temp_node = temp_node->next;
             }
-            addTail(new_node);
-            return true;
         }
     }
-    return false;
 }
 
 bool List::deleteNode(int){
@@ -43,6 +47,7 @@ bool List::getNode(int, DataNode *){
 void List::printList(bool){
     Node *temp_node;
     temp_node = head;
+    cout << "========== Printing List ==========" << endl;
     for (int i = 0; i < count; i++) {
         cout << "\tNode Number: " << (i+1) << endl;
         cout << "ID: " << temp_node->id << endl;
@@ -73,8 +78,16 @@ int List::searchIds(int id_search){
     return count;
 }
 
-void List::getNodePos(int id_search, string temp_data){
-
+int List::getNodePos(int id_search){
+    Node *temp_node = head;
+    for (int i = 0; i < count; i++) {
+        if(temp_node->id < id_search && temp_node->next->id > id_search){
+            return i+1;
+        }else{
+            temp_node = temp_node->next;
+        }
+    }
+    return -1;
 }
 
 void List::addHead(Node *temp_node){
@@ -97,8 +110,17 @@ void List::deleteHead(){
 
 }
 
-void List::addMiddle(Node *temp_node){
-
+void List::addMiddle(Node *new_node){
+    Node *temp_node = head;
+    int node_pos = getNodePos(new_node->id);
+    for (int i = 0; i < node_pos; i++) {
+        temp_node = temp_node->next;
+    }
+    new_node->next = temp_node;
+    new_node->back = temp_node->back;
+    temp_node->back->next = new_node;
+    temp_node->back = new_node;
+    count++;
 }
 
 void List::deleteMiddle(){
